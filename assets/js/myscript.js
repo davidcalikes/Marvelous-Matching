795c4d728c32a3e29858990119451c71a97abc5d
davidcalikes <davidcalikes@gmail.com>
2022-06-07 22:49:15 +0000

    feat: Add a function to myscript.js that plays a sound and opens the shapes page on click event

--- assets/js/myscript.js
+++ assets/js/myscript.js
@@ -10,3 +10,18 @@
       location.href = 'animals.html';
     });
   }
+
+  /** 
+ * Plays shapes button sound when button is clicked and loads animals game page
+ * Called from the DOM via onclick event
+ * Waits until sound has ended before loading page to prevent audio clipping
+ * */
+function playShapes() {
+    let sound = document.getElementById("shapes");
+    sound.play();
+    sound.addEventListener('ended', function () {
+      location.href = 'shapes.html';
+    });
+  }
+
+  
